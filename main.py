35725364729e057143e9259e4b04d278effd94a7
Abraham Tehseen <107781862+PersonXXIII@users.noreply.github.com>
2023-06-17 11:17:50 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,123 @@
+from flask import Flask, request, render_template
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from flask_restful import Api, Resource
+import google.auth
+from google.cloud import translate_v2 as translate
+from google.oauth2 import service_account
+import tensorflow as tf
+import numpy as np
+import json
+import string
+import random
+import os
+
+# Load credentials from JSON file
+credentials = service_account.Credentials.from_service_account_file('API-KEY.json')
+
+# Authenticate the Translation API client
+translate_client = translate.Client(credentials=credentials)
+
+
+# load the tokenizer and label encoder
+with open('tokenizer.json') as f:
+    data = json.load(f)
+    tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(data)
+
+with open('label_encoder.json') as f:
+    data = json.load(f)
+    le = LabelEncoder()
+    le.classes_ = np.array(data)
+
+with open('responses.json') as file:
+    responses = json.load(file)
+
+# load the trained model
+model = tf.keras.models.load_model('my_model.h5')
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/', methods=['GET'])
+def info():
+    return render_template('info.html')
+
+class Chatbot(Resource):
+    def post(self):
+        try:
+            # get user input
+            data = request.get_json()
+            texts_p = []
+            prediction_input = data['input']
+            input_lang = translate_client.detect_language(prediction_input)['language']
+
+            with open('special_words.json', 'r') as f:
+                special_words = json.load(f)    
+
+            if prediction_input == "" or prediction_input in special_words:
+                input_lang='en'
+
+            if input_lang in ['en', 'ur']:            
+                if input_lang != 'en':
+                    prediction_input = translate_client.translate(prediction_input, target_language='en')['translatedText']
+
+                # preprocess the input
+                prediction_input = prediction_input.lower() # convert to lowercase
+                prediction_input = ''.join(c for c in prediction_input if c not in string.punctuation) # remove punctuation
+                prediction_input = ' '.join(prediction_input.split())
+                
+                # Replacing words using a dictionary
+                with open("dictionary.json", "r") as f:
+                    replacement_dict = json.load(f)
+
+                with open("Other_words.json", "r") as f:
+                    Other_words = json.load(f)
+
+                if prediction_input in Other_words:
+                    pass
+                else:
+                    prediction_words = prediction_input.split()
+                    prediction_words = [replacement_dict.get(word, word) for word in prediction_words]
+                    prediction_input = ' '.join(prediction_words)
+
+                texts_p.append(prediction_input)
+                prediction_input = tokenizer.texts_to_sequences(texts_p)
+                prediction_input = np.array(prediction_input).reshape(-1)
+                prediction_input = pad_sequences([prediction_input], maxlen=88)
+
+                # get the output from the model
+                output = model.predict(prediction_input)
+                output = output.argmax()
+
+                # find the tag and select a response
+                response_tag = le.inverse_transform([output])[0]
+                response = random.choice(responses[response_tag])
+
+                if input_lang != 'en':
+                    delimiter = '|||'
+                    response = response.replace('\n', delimiter)
+                    translated_response = translate_client.translate(response, target_language=input_lang)['translatedText']
+                    if input_lang=='ur':
+                        response = translated_response.replace('||', '\n')
+                    else:
+                        response = translated_response.replace(delimiter, '\n')
+
+            else:
+                response="Doesn't Support the language you are using."
+                if input_lang != 'en':
+                    response = translate_client.translate(response, target_language=input_lang)['translatedText']
+
+             # return the response
+            return {'response': response}
+
+
+        except Exception as e:
+            return {'response' : "Doesn't Support the language you are using. ERROR!"}
+
+api.add_resource(Chatbot, '/chatbot')
+
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8888))
+    app.run(host='0.0.0.0', port=port)
